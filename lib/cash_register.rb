eba5bd056898e57e4889166679255b8f40366f8c
Paul Rosero <paul_rosero@yahoo.com>
2019-05-10 01:55:16 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
+class CashRegister
+attr_accessor :total, :employee_discount :items
 
+
+  def initialize(employee_discount = nil)
+    @total = 0.00
+    @employee_discount = employee_discount
+  end
+  
+   def discount
+    self.employee_discount
+   end
+   
+   def items
+     @items
+   end
+  
+   def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times do
+      items << title
+    end
+  end
+
+end
